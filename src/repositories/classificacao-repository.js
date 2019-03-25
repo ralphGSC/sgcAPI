1eb9107a7d7f451fa6274b5d32565edf3193f1f2
ralphGSC <47863700+ralphGSC@users.noreply.github.com>
2019-03-25 09:52:16 -0300

    only get classificacao

--- src/repositories/classificacao-repository.js
+++ src/repositories/classificacao-repository.js
@@ -8,29 +8,29 @@ exports.get = async (data) => {
     return res.rows;
 }
 
-exports.post = async (data) => {  
-    const res = await db.comandText(`INSERT INTO SGC_CLASSIFICACAO 
-                                    (SGC_ID, SGC_DESC_CLASSIFICACAO)
-                                    VALUES
-                                    (${data.SGC_ID}, '${data.SGC_DESC_CLASSIFICACAO}')`);   
+// exports.post = async (data) => {  
+//     const res = await db.comandText(`INSERT INTO SGC_CLASSIFICACAO 
+//                                     (SGC_ID, SGC_DESC_CLASSIFICACAO)
+//                                     VALUES
+//                                     (${data.SGC_ID}, '${data.SGC_DESC_CLASSIFICACAO}')`);   
    
-    return res;
-}
+//     return res;
+// }
 
-exports.put = async (sgc_id, data) => {      
-    const res = await db.comandText(`UPDATE SGC_CLASSIFICACAO SET
-                                            SGC_DESC_CLASSIFICACAO = '${data.SGC_DESC_CLASSIFICACAO}' 
-                                     WHERE 1=1
-                                       AND SGC_ID = ${sgc_id}`);   
+// exports.put = async (sgc_id, data) => {      
+//     const res = await db.comandText(`UPDATE SGC_CLASSIFICACAO SET
+//                                             SGC_DESC_CLASSIFICACAO = '${data.SGC_DESC_CLASSIFICACAO}' 
+//                                      WHERE 1=1
+//                                        AND SGC_ID = ${sgc_id}`);   
    
-    return res;
-}
+//     return res;
+// }
 
-exports.delete = async (sgc_id) => { 
-    console.log(sgc_id);
-    const res = await db.comandText(`DELETE FROM SGC_CLASSIFICACAO
-                                      WHERE 1=1
-                                        AND SGC_ID = ${sgc_id}`);   
+// exports.delete = async (sgc_id) => { 
+//     console.log(sgc_id);
+//     const res = await db.comandText(`DELETE FROM SGC_CLASSIFICACAO
+//                                       WHERE 1=1
+//                                         AND SGC_ID = ${sgc_id}`);   
    
-    return res;
-}
+//     return res;
+// }
