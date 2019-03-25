4d9de6d0d7be4298e288f02aa3dac1b5cb386003
ralphGSC <47863700+ralphGSC@users.noreply.github.com>
2019-03-25 09:54:13 -0300

    only get classificacao 2

--- src/routes/classificacao-route.js
+++ src/routes/classificacao-route.js
@@ -6,8 +6,8 @@ const controller = require('../controllers/classificacao-controller');
 const authService = require('../services/auth-service');
 
 router.get('/', authService.authorize, controller.get);
-router.post('/', authService.authorize, controller.post);
-router.put('/:sgc_id', authService.authorize, controller.put);
-router.delete('/:sgc_id', authService.authorize, controller.delete);
+//router.post('/', authService.authorize, controller.post);
+//router.put('/:sgc_id', authService.authorize, controller.put);
+//router.delete('/:sgc_id', authService.authorize, controller.delete);
 
 module.exports = router;
