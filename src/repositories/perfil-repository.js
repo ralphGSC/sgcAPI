1ff56ab020d4fc09002e01a092ef01df1b3057aa
ralphGSC <47863700+ralphGSC@users.noreply.github.com>
2019-03-27 11:28:53 -0300

    perfil

--- src/repositories/perfil-repository.js
+++ src/repositories/perfil-repository.js
@@ -4,7 +4,8 @@ const db = require('../services/database-service');
 
 
 exports.get = async (cod_usuario) => { 
-    const res = await db.comandText(`SELECT * FROM SGC_USUARIO_PERFIL
+    const res = await db.comandText(`SELECT COD_PERFIL, DATA_CADASTRO
+                                       FROM SGC_USUARIO_PERFIL
                                       WHERE 1=1
                                         AND COD_USUARIO = ${cod_usuario}`);
     return res.rows;
